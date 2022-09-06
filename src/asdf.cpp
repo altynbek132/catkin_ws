ce0a2429d17efebb6cd25f8ca3616127d676b317
Your Name <you@example.com>
2022-09-06 13:33:15 +0600

    slide code

---
+++ src/asdf.cpp
@@ -0,0 +1,19 @@
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
+
+#include <math.h>
+
+int main (int argc, char **argv) {
+	ros::init(argc, argv, "rotate");
+	ros::NodeHandle nh;
+	ros::Rate loop_rate(10);
+	ros::Time startTime = ros::Time::now();
+	while(ros::ok()) {
+		std_msgs::Float64 msg_to_send;
+		msg_to_send.data = 0.0;
+		pub3_publish(msg_to_send);
+        ROS_INFO("ismoving goalpos to zero");
+        ros::spinOnce();
+        loop_rate.sleep();
+	}
+}
